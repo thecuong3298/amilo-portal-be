a7049b17b021f92a94abec58c9842115f40ed9c5
cuongdt <thecuong4298@gmail.com>
2022-04-18 00:20:01 +0700

    init

---
+++ src/main/java/com/amilo/portal/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.amilo.portal.repository;
+
+import com.amilo.portal.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
---
+++ src/main/java/com/amilo/portal/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.amilo.portal.service.dto;
